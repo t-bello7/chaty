05bc4d46a62dbb98218f1867c20b2b8ee655e6ee
Bell Oluwatomisin <bello4aus@gmail.com>
2021-12-23 19:48:37 +0100

    removed receiptsview

--- message/urls.py
+++ message/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('room/',RoomAPIView.as_view(), name='room'),
     path('room/<pk>/',RoomAPIView.as_view(), name=''),
     path('message/', MessagerAPIView.as_view(), name='message'),
-    path('read_receipts', ReceiptsAPIView.as_view(), name='read_receipts')
+    # path('read_receipts', ReceiptsAPIView.as_view(), name='read_receipts')
 ]
